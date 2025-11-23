631a18260facad82b2425e7edcff6f1b05b6c753
abhijitc18 <chouguleabhijit18@gmail.com>
2025-11-24 00:00:59 +0530

    update: add new components in the app.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import HeroSlider from "./components/HeroSlider";
 import About from "./sections/About";
+import VisionMission from "./sections/VisionMission";
 import Services from "./sections/Services";
 import OurPartner from "./sections/OurPartner";
 import TradingViewWidget from "./components/TradingViewWidget";
 import Client from "./sections/Client";
-import Contact from "./sections/Contact";
+// import Contact from "./sections/Contact";
+import Gallery from "./sections/Gallery";
+import KeyFeatures from "./sections/KeyFeatures";
+import HowItWorks from "./sections/HowItWorks";
 import TradingViewTicker from "./components/TradingViewTicker";
 import Footer from "./components/Footer";
+import FAQ from "./sections/FAQ";
 import FloatingButtons from "./components/FloatingButtons";
-import "./App.css";
 import PopupModel from "./components/PopupModal";
 import partners from "./data/partners";
+import "./App.css";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -24,11 +29,16 @@ function App() {
         <HeroSlider />
         <TradingViewTicker />
         <About />
+        <VisionMission />
         <Services />
         <OurPartner openModal={() => setShowModal(true)} />
         <TradingViewWidget />
         <Client />
-        <Contact />
+        {/* <Contact /> */}
+        <Gallery />
+        <KeyFeatures />
+        <HowItWorks />
+        <FAQ />
         <PopupModel
           show={showModal}
           onClose={() => setShowModal(false)}
